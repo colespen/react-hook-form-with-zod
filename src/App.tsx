7e9323a87c050ad8d491880486d48a40582030a3
Spencer Cole <spencerlewiscole@gmail.com>
2023-06-25 13:25:27 -0400

    build a validatoinScehem with zod and assign a typescript type

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import Form from "./components/Form";
 function App() {
   return (
     <>
-      <Form />
+      <div className="max-w-xl mx-auto w-full">
+      <div className="flex justify-center my-12">
+        <div className="bg-[#3c3b48] w-full lg:w-12/12 p-5 rounded-lg shadow-xl shadow-black">
+          <h3 className="pt-4 pb-4 text-2xl text-center font-bold text-[#d1d1d1]">
+            Create New Account
+          </h3>
+          <Form />
+        </div>
+      </div>
+    </div>
     </>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,35 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const validationSchema = z
+  .object({
+    firstName: z.string().min(1, { message: "Firstname is required" }),
+    lastName: z.string().min(1, { message: "Lastname is required" }),
+    email: z.string().min(1, { message: "Email is required" }).email({
+      message: "Must be a valid email",
+    }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be atleast 6 characters" }),
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Confirm Password is required" }),
+    terms: z.literal(true, {
+      // every schema can have error map, used to customize err msg
+      errorMap: () => ({ message: "You must accept Terms and Conditions" }),
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password don't match",
+    path: ["confirmPassword"],
+  });
+
+type validationSchema = z.infer<typeof validationSchema>;
+
 const Form = () => {
   return (
-    <form className="bg-[#333247] px-8 pt-6 pb-8 mb-4">
+    <form className="px-8 pt-6 pb-8 mb-4">
       <div className="mb-4 md:flex md:justify-between">
         <div className="mb-4 md:mr-2 md:mb-0">
           <label
@@ -10,7 +39,7 @@ const Form = () => {
             First Name
           </label>
           <input
-            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-slate-700 focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
+            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-[#444351] focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
             id="firstName"
             type="text"
             placeholder="First Name"
@@ -24,7 +53,7 @@ const Form = () => {
             Last Name
           </label>
           <input
-            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-slate-700 focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
+            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-[#444351] focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
             id="lastName"
             type="text"
             placeholder="Last Name"
@@ -39,7 +68,7 @@ const Form = () => {
           Email
         </label>
         <input
-          className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-slate-700 focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
+          className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-[#444351] focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
           id="email"
           type="email"
           placeholder="Email"
@@ -54,7 +83,7 @@ const Form = () => {
             Password
           </label>
           <input
-            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-slate-700 focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
+            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-[#444351] focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline placeholder:text-slate-400"
             id="password"
             type="password"
           />
@@ -67,24 +96,24 @@ const Form = () => {
             Confirm Password
           </label>
           <input
-            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-slate-700 focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline"
+            className="bg-[#242424] w-full px-3 py-2 text-sm leading-tight text-[#d1d1d1] border-2 border-[#444351] focus:border-slate-600 rounded appearance-none focus:outline-none focus:shadow-outline"
             id="c_password"
             type="password"
           />
         </div>
       </div>
       <div className="mb-4">
-        <input type="checkbox" id="terms" className="bg-[#242424]"/>
+        <input type="checkbox" id="terms" className="bg-[#242424]" />
         <label
           htmlFor="terms"
-          className= "ml-2 mb-2 text-sm font-bold text-[#d1d1d1]"
+          className="ml-2 mb-2 text-sm font-bold text-[#d1d1d1]"
         >
           Accept Terms & Conditions
         </label>
       </div>
       <div className="mb-6 text-center">
         <button
-          className="w-full px-4 py-2 font-bold text-white bg-blue-600 rounded-full hover:bg-blue-800 focus:outline-none focus:shadow-outline"
+          className="w-full px-4 py-2 font-bold text-white bg-blue-800 rounded-full hover:bg-blue-900 focus:outline-none focus:shadow-outline"
           type="submit"
         >
           Register Account
